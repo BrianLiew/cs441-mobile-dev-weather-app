4fbfe538cf681eb59921e8b69623965e2fcce988
Brian Liew <brianliew@Brians-MacBook-Air.local>
2021-10-27 13:25:08 -0400

    added more labels and changed theme from pink to green

--- weather app/weather app/ViewController.swift
+++ weather app/weather app/ViewController.swift
@@ -23,10 +23,13 @@ class ViewController: UIViewController {
     // UIImageView
     let weather_graphic = UIImageView(frame: CGRect(x: screen_width / 2 - 150, y: screen_height / 4, width: 300, height: 150))
     // UILabel
-    let city_label = UILabel(frame: CGRect(x: screen_width / 2 - 150, y: screen_height / 4 - 150, width: 300, height: 100))
+    let city_label = UILabel(frame: CGRect(x: 0, y: screen_height / 4 - 150, width: screen_width, height: 100))
+    let temp_title = UILabel(frame: CGRect(x: screen_width / 2 - 150, y: screen_height / 2, width: 300, height: 50))
     let temp_label = UILabel(frame: CGRect(x: screen_width / 2 - 150, y: screen_height / 2 + 50, width: 300, height: 100))
     let temp_max_label = UILabel(frame: CGRect(x: screen_width / 2 - 150, y: screen_height / 2 + 200, width: 100, height: 50))
+    let temp_max_title = UILabel(frame: CGRect(x: screen_width / 2 - 150, y: screen_height / 2 + 150, width: 100, height: 50))
     let temp_min_label = UILabel(frame: CGRect(x: screen_width / 2 + 50, y: screen_height / 2 + 200, width: 100, height: 50))
+    let temp_min_title = UILabel(frame: CGRect(x: screen_width / 2 + 50, y: screen_height / 2 + 150, width: 100, height: 50))
     
     // MARK: JSONDecoder declaration
     
@@ -37,65 +40,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         init_UI()
-        // request weather data
+
         let network_instance = Networking()
         network_instance.make_request(completion_handler: request_completion_handler)
     }
     
-    // MARK: custom functions
+    // MARK: UI functions
     
     func init_UI() {
-        top_background.backgroundColor = UIColor.purple
-        bottom_background.backgroundColor = UIColor.systemPink
+        top_background.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 0.5)
+        bottom_background.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 0.5)
         weather_graphic.backgroundColor = UIColor.white
-        city_label.backgroundColor = UIColor.white
-        temp_label.backgroundColor = UIColor.purple
-        temp_max_label.backgroundColor = UIColor.red
-        temp_min_label.backgroundColor = UIColor.blue
+        city_label.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 0.5)
+        temp_label.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 0.5)
+        temp_max_label.backgroundColor = UIColor(red: 0.5, green: 0.6, blue: 0, alpha: 0.5)
+        temp_min_label.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0.5, alpha: 0.5)
         
         city_label.text = "-"
+        temp_title.text = "Temperature"
         temp_label.text = "0.00 °F"
         temp_max_label.text = "0.00 °F"
+        temp_max_title.text = "Maximum"
         temp_min_label.text = "0.00 °F"
+        temp_min_title.text = "Minimum"
         
         city_label.textAlignment = .center
         temp_label.textAlignment = .center
+        temp_title.textAlignment = .center
         temp_max_label.textAlignment = .center
+        temp_max_title.textAlignment = .center
         temp_min_label.textAlignment = .center
+        temp_min_title.textAlignment = .center
         
         city_label.font = big_font
         temp_label.font = big_font
+        temp_title.font = big_font
         temp_max_label.font = small_font
+        temp_max_title.font = small_font
         temp_min_label.font = small_font
+        temp_min_title.font = small_font
         
         view.addSubview(top_background)
         view.addSubview(bottom_background)
         view.addSubview(weather_graphic)
         view.addSubview(city_label)
         view.addSubview(temp_label)
+        view.addSubview(temp_title)
         view.addSubview(temp_max_label)
+        view.addSubview(temp_max_title)
         view.addSubview(temp_min_label)
-    }
-    
-    // completion handler for make_request(). parser from Data? -> [String: AnyObject]
-    func request_completion_handler(json: Data?) -> Void {
-        if let json_unwrapped = json {
-            DispatchQueue.main.async {
-                let weather_data = try? self.decoder.decode(weather_data.self, from: json_unwrapped)
-                if let weather_data_unwrapped = weather_data {
-                    self.update_UI(
-                        city_name: weather_data_unwrapped.name,
-                        temp_double: weather_data_unwrapped.main.temp,
-                        temp_max_double: weather_data_unwrapped.main.temp_max,
-                        temp_min_double: weather_data_unwrapped.main.temp_min
-                    )
-                }
-                
-            }
-        } else {
-            print("json unwrap error")
-        }
+        view.addSubview(temp_min_title)
     }
     
     func update_UI(
@@ -108,6 +104,25 @@ class ViewController: UIViewController {
         self.temp_label.text = String(format: "%.1f", convert_kelvin_to_fahrenheit(input: temp_double)) + "°F"
         self.temp_max_label.text = String(format: "%.1f", convert_kelvin_to_fahrenheit(input: temp_max_double)) + "°F"
         self.temp_min_label.text = String(format: "%.1f", convert_kelvin_to_fahrenheit(input: temp_min_double)) + "°F"
+    }
+    
+    // MARK: data function
+    
+    // completion handler for make_request(). parser.
+    func request_completion_handler(json: Data?) -> Void {
+        if let json_unwrapped = json {
+            DispatchQueue.main.async {
+                let weather_data = try? self.decoder.decode(weather_data.self, from: json_unwrapped)
+                if let weather_data_unwrapped = weather_data {
+                    self.update_UI(
+                        city_name: weather_data_unwrapped.name,
+                        temp_double: weather_data_unwrapped.main.temp,
+                        temp_max_double: weather_data_unwrapped.main.temp_max,
+                        temp_min_double: weather_data_unwrapped.main.temp_min
+                    )
+                }
+            }
+        } else { print("json unwrap error") }
     }
     
 }
